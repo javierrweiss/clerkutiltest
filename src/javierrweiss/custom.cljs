c58ede40d3ab42027da89fddc6fe772f4a48d031
javierrweiss <javierweiss@gmail.com>
2023-03-29 16:14:36 +0000

    initial commit

---
+++ src/javierrweiss/custom.cljs
@@ -0,0 +1,15 @@
+(ns javierrweiss.custom)
+
+;; ## Custom ClojureScript
+;;
+;; This namespace contains custom ClojureScript definitions used by the
+;; {{raw/name}} project.
+
+;; With the default configuration in `javierrweiss.sci-extensions`, any function
+;; or var you add here will be available inside your SCI viewers as
+;; `javierrweiss.custom/<fname>`.
+
+(defn square
+  "Returns the square of `x`."
+  [x]
+  (* x x))
